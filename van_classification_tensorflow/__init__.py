14b2067a3f2f485dfb33112b9f698fe29a9707ba
EMalagoli92 <emala.892@gmail.com>
2023-02-04 16:45:36 +0100

    Create __init__

---
+++ van_classification_tensorflow/__init__.py
@@ -0,0 +1,26 @@
+__author__ = "EMalagoli92"
+import json
+import os
+import random
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.experimental.numpy as tnp
+from tensorflow.python.framework import random_seed
+
+tnp.experimental_enable_numpy_behavior()
+
+# Set Seed
+SEED = 123
+os.environ["PYTHONHASHSEED"] = str(SEED)
+random.seed(SEED)
+tf.random.set_seed(SEED)
+random_seed.set_seed(SEED)
+np.random.seed(SEED)
+
+with open(
+    os.path.normpath(os.path.join(os.path.dirname(__file__), "version.json")), "r"
+) as handle:
+    __version__ = json.load(handle)["version"]
+
+from .models.van import VAN
