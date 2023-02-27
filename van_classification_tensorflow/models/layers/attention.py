346ec4a757dbf7e20219c963ec72d4e923b09d36
EMalagoli92 <emala.892@gmail.com>
2023-02-27 22:21:40 +0100

    Update attention.py

--- van_classification_tensorflow/models/layers/attention.py
+++ van_classification_tensorflow/models/layers/attention.py
@@ -10,6 +10,7 @@ from van_classification_tensorflow.models.layers.utils import (
 
 @tf.keras.utils.register_keras_serializable(package="van")
 class Attention(tf.keras.layers.Layer):
+    """ Basic attention module in VAN Block."""
     def __init__(self, d_model: int, **kwargs):
         """
         Parameters
